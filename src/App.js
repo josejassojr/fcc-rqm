89bc5df75f04a34d52971685234b4cc1d11fb4ed
josejassojr <jose.d.jassojr@gmail.com>
2021-09-10 17:22:26 -0700

    Works, needs styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import { Component } from "react";
+import { QUOTES } from "./quotes";
 
-class App extends Component {
+const lenQuotes = QUOTES.length;
+
+class QuoteBox extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      index: Math.floor(Math.random() * lenQuotes)
+    };
+    this.getNewQuote = this.getNewQuote.bind(this);
   }
+
+  getNewQuote() {
+    this.setState({
+      index: Math.floor(Math.random() * lenQuotes)
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header> Random Quote Machine </header>
-        <div id="quote-box">
-          Quote Box
+      <div className="QuoteBox" id="quote-box">
+        <div id="text"><i class="fas fa-quote-left"></i>  {QUOTES[this.state.index].quote}
         </div>
+        <div id="author">-{QUOTES[this.state.index].author}</div>
+        <button id="new-quote" onClick={this.getNewQuote}>
+          New Quote
+        </button>
+        <button>
+          <a
+            id="tweet-quote"
+            target="_blank"
+            href={
+              "https://twitter.com/intent/tweet?text=" +
+              QUOTES[this.state.index].quote.split(" ").join("%20") +
+              " -" +
+              QUOTES[this.state.index].author.split(" ").join("%20") +
+              "&hashtags=FCC"
+            }
+          >
+            <i class="fab fa-twitter"></i>
+          </a>
+        </button>
       </div>
     );
   }
 }
 
-export default App;
+export default QuoteBox;
---
+++ src/quotes.js
@@ -0,0 +1,31 @@
+export const QUOTES = [
+  {
+    author: "Mark Twain",
+    quote: "The man who does not read good books has no advantage over the man who cannot read them."
+  },
+  {
+    author: "Albert Einstein",
+    quote: "God does not play dice with the universe."
+  },
+  {
+    author: "Edgar Allan Poe",
+    quote: "All that we see or seem is but a dream within a dream."
+  },
+  {
+    author: "Douglas MacArthur",
+    quote: "Only those are fit to live who are not afraid to die."
+  },
+  {
+    author: "Winston Churchill",
+    quote: "If you are going through hell, keep going."
+  },
+  {
+    author: "Eleanor Roosevelt",
+    quote: "If life were predictable it would cease to be life, and be without flavor."
+  },
+  {
+    author: "Aristotle",
+    quote: "It is during our darkest moments that we must focus to see the light."
+  }
+];
+
